b2816221e2b27a00d11f14d0dffba54a8856532c
YN <julia.nikit.89@gmail.com>
2026-05-02 13:01:32 +0200

    change text and icon in button favorite

--- src/js/modal-exercise.js
+++ src/js/modal-exercise.js
@@ -1,6 +1,18 @@
 import { getExerciseById, addRating } from './api.js';
 import { STORAGE_KEYS } from './constants.js';
 
+const heartIcon = `
+<svg width="19" height="17" viewBox="0 0 19 17" fill="none">
+  <path d="M16.8261 2.09319C16.4004 1.66736 15.8951 1.32956 15.3389 1.09909C14.7827 0.868623 14.1865 0.75 13.5844 0.75C12.9823 0.75 12.3862 0.868623 11.8299 1.09909C11.2737 1.32956 10.7684 1.66736 10.3427 2.09319L9.45941 2.97652L8.57607 2.09319C7.71633 1.23344 6.55027 0.750446 5.33441 0.750446C4.11855 0.750446 2.95249 1.23344 2.09274 2.09319C1.233 2.95293 0.75 4.11899 0.75 5.33485C0.75 6.55072 1.233 7.71678 2.09274 8.57652L2.97607 9.45986L9.45941 15.9432L15.9427 9.45986L16.8261 8.57652C17.2519 8.15089 17.5897 7.64553 17.8202 7.08932C18.0506 6.5331 18.1693 5.93693 18.1693 5.33485C18.1693 4.73278 18.0506 4.13661 17.8202 3.58039C17.5897 3.02418 17.2519 2.51882 16.8261 2.09319Z" stroke="#242424" stroke-width="1.5"/>
+</svg>
+`;
+
+const cartIcon = `
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+  <path d="M13.3333 4.99996V4.33329C13.3333 3.39987 13.3333 2.93316 13.1517 2.57664C12.9919 2.26304 12.7369 2.00807 12.4233 1.84828C12.0668 1.66663 11.6001 1.66663 10.6667 1.66663H9.33333C8.39991 1.66663 7.9332 1.66663 7.57668 1.84828C7.26308 2.00807 7.00811 2.26304 6.84832 2.57664C6.66667 2.93316 6.66667 3.39987 6.66667 4.33329V4.99996M8.33333 9.58329V13.75M11.6667 9.58329V13.75M2.5 4.99996H17.5M15.8333 4.99996V14.3333C15.8333 15.7334 15.8333 16.4335 15.5608 16.9683C15.3212 17.4387 14.9387 17.8211 14.4683 18.0608C13.9335 18.3333 13.2335 18.3333 11.8333 18.3333H8.16667C6.76654 18.3333 6.06647 18.3333 5.53169 18.0608C5.06129 17.8211 4.67883 17.4387 4.43915 16.9683C4.16667 16.4335 4.16667 15.7334 4.16667 14.3333V4.99996" stroke="#242424" stroke-width="1.5"/>
+</svg>
+`;
+
 const modal = document.querySelector('[data-modal]');
 const modalContent = document.querySelector('[data-modal-content]');
 const closeBtn = document.querySelector('[data-modal-close]');
@@ -155,10 +167,15 @@ function toggleFavorite(exercise) {
 }
 
 function renderModal(exercise) {
-  const favouritesButtonText = isFavorite(exercise._id)
-    ? 'Unfavorite'
-    : 'Add to favorites';
-  const GiveARating = "Give a rating"
+  const isFav = isFavorite(exercise._id);
+
+const favouritesButtonText = isFav
+  ? 'Remove from favorites'
+  : 'Add to cart';
+
+const icon = isFav ? cartIcon : heartIcon;
+
+const GiveARating = "Give a rating";
 
 const exactRating = Number(exercise.rating || 0).toFixed(1);
   const ratingValueRound = Math.round(exercise.rating || 0); 
@@ -205,10 +222,10 @@ const exactRating = Number(exercise.rating || 0).toFixed(1);
         <p>${exercise.description}</p>
 
         <div class="modal-actions">
-          <button type="button" class="btn-fav" data-fav-btn>${favouritesButtonText} 
-          <svg width="19" height="17" viewBox="0 0 19 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M16.8261 2.09319C16.4004 1.66736 15.8951 1.32956 15.3389 1.09909C14.7827 0.868623 14.1865 0.75 13.5844 0.75C12.9823 0.75 12.3862 0.868623 11.8299 1.09909C11.2737 1.32956 10.7684 1.66736 10.3427 2.09319L9.45941 2.97652L8.57607 2.09319C7.71633 1.23344 6.55027 0.750446 5.33441 0.750446C4.11855 0.750446 2.95249 1.23344 2.09274 2.09319C1.233 2.95293 0.75 4.11899 0.75 5.33485C0.75 6.55072 1.233 7.71678 2.09274 8.57652L2.97607 9.45986L9.45941 15.9432L15.9427 9.45986L16.8261 8.57652C17.2519 8.15089 17.5897 7.64553 17.8202 7.08932C18.0506 6.5331 18.1693 5.93693 18.1693 5.33485C18.1693 4.73278 18.0506 4.13661 17.8202 3.58039C17.5897 3.02418 17.2519 2.51882 16.8261 2.09319Z" stroke="#242424" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-</svg></button>
+          <button type="button" class="btn-fav" data-fav-btn>
+  ${favouritesButtonText}
+  ${icon}
+</button>
           <button type="button" class="btn-rating">${GiveARating}</button>
         </div>
       </div>
@@ -270,3 +287,4 @@ document.addEventListener('keydown', event => {
     }
   }
 });
+
